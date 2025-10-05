0e23e57e4d6681e9f05513e57859b0ed972448b4
singhdevendrajtz <singhdevendrajtz@gmail.com>
2025-10-05 22:22:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 //your JS code here. If required.
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
-
+let prom_arr=[]
 const images = [
   { url: "https://picsum.photos/id/237/200/300" },
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+let new_el=document.createElement("div");
+new_el.id="loading";
+new_el.class="spinner"
+new_el.innerHTML="Loading..."
+output.appendChild(new_el)
+
+for(let x=0;x<images.length;x++)
+	{
+	 prom_arr.push( fetch(images[x].url))
+	}
+//console.log(prom_arr)
+
+Promise.all(prom_arr).then((data)=>{
+	output.innerHTML="";
+	for(let x=0;x<data.length;x++)
+		{
+		let src=data[x].url;
+			//console.log(src)
+			let el=document.createElement("img");
+			el.style.width="200px";
+			el.style.height="300px";
+			el.src=`${src}`
+			output.appendChild(el)
+			output.innerHTML
+		}
+}).catch((error)=>{
+	
+	let elems=document.createElement("div");
+	elems.id="error"
+	elems.innerHTML=`${error}`
+	output.appendChild(elems);
+});
